64a0342918b81847240c55b38151c07956bf19b1
Ryan <drn@ryans-mac.local>
2018-10-24 21:47:12 -0700

    Initial Commit

---
+++ TapBooster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapBooster
+//
+//  Created by Ryan on 2018-10-24.
+//  Copyright © 2018 DrN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
