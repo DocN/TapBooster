effee50560afce3d0491989b6ed5449deb7bf45e
Ryan <031247ryan@gmail.com>
2018-12-01 18:54:41 -0800

    daily drive

--- TapBooster/SingleplayerController.swift
+++ TapBooster/SingleplayerController.swift
@@ -15,39 +15,66 @@ class SingleplayerController: UIViewController {
     
     var scoreValue = 0;
     var scoreTitle = "Score: ";
-    var levelValue = 0;
+    var levelScore = 0;
+    var levelValue = 1;
+    var levelUpCap = 50;
+    
     var timeValue = 0;
     var acuracyValue = 0;
+    var speed = 1.0;
+    var timerCount = 1.0;
 
+    var timer = Timer.scheduledTimer(timeInterval: speed, target: self, selector: #selector(generateTarget), userInfo: nil, repeats: true)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(generateTarget), userInfo: nil, repeats: true)
+        let countTime = Timer.scheduledTimer(timeInterval: timerCount, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
+    
     @objc func generateTarget() {
         let button = UIButton();
         button.setTitle("Add", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         button.setImage(UIImage(named: "target.png"), for: .normal)
         let screenSize = UIScreen.main.bounds
-        let randomXPos = CGFloat.random(in: 0..<(screenSize.width - 50))
-        let randomYPos = CGFloat.random(in: 300..<(screenSize.height - 50))
+        let btnWidth = CGFloat.random(in: 20..<(100))
+        let randomXPos = CGFloat.random(in: 0..<(screenSize.width - btnWidth))
+        let randomYPos = CGFloat.random(in: 300..<(screenSize.height - btnWidth))
         button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
-        button.frame = CGRect(x: randomXPos, y: randomYPos, width: 50, height: 50)
+        
+        button.frame = CGRect(x: randomXPos, y: randomYPos, width: btnWidth, height: btnWidth)
 
         self.view.addSubview(button)
         
         print("Timer fired!")
     }
     
+    @objc func updateTimer() {
+        timeValue=timeValue+1;
+        time.text="Time: "+String(timeValue);
+    }
 
     @objc func buttonClicked(sender : UIButton){
         scoreValue = scoreValue+1;
+        levelScore = levelScore+1;
         score.text = scoreTitle+String(scoreValue);
+        checkLevelUp();
         sender.isHidden = true;
     }
     
+    func checkLevelUp() {
+        if(levelScore >= 10) {
+            levelValue=levelValue+1;
+            print("Level up");
+            speed=(speed/1.9);
+            timer.invalidate();
+            timer = Timer.scheduledTimer(timeInterval: speed, target: self, selector: #selector(generateTarget), userInfo: nil, repeats: true)
+            levelScore = 0;
+        }
+        level.text = "Level: "+String(levelValue);
+    }
     func clearScore() {
         scoreValue = 0;
         levelValue = 0;
